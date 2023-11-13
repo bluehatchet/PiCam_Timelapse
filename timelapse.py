c8ee1f379dbdf20e38f7f745303f6a5245f51083
Justin <146759190+bluehatchet@users.noreply.github.com>
2023-11-13 15:51:32 -0500

    Update timelapse.py
    * Correct main buttons to be displayed at the bottom left of the display and forward-facing before the preview window. 
    * Ensured that a selection window was centered with the amount intervals in seconds from 10secs-24hours was available.

--- timelapse.py
+++ timelapse.py
@@ -1,11 +1,11 @@
 import tkinter as tk
-from tkinter import simpledialog
+from tkinter import Toplevel
 import os
 import subprocess
 from time import sleep
 
 # The directory to store the images
-IMAGE_DIR = "/directory/to/save/images"
+IMAGE_DIR = "/path/to/save/images"
 
 # Check if the image directory exists, if not create it
 if not os.path.exists(IMAGE_DIR):
@@ -30,11 +30,32 @@ def stop_time_lapse():
     running = False
 
 def set_interval():
-    global interval
-    interval = simpledialog.askinteger("Input", "Enter the time interval in seconds",
-                                      parent=root)
-    if interval is None:
-        interval = 1  # Default to 1 second if no input is provided
+    def select_interval(n):
+        global interval
+        interval = n
+        interval_popup.destroy()
+
+    # Create a new pop-up window
+    interval_popup = Toplevel(root)
+    interval_popup.title("Select Interval")
+
+    # Center the pop-up window on the screen
+    interval_popup.geometry("+%d+%d" % (root.winfo_screenwidth() / 2 - interval_popup.winfo_reqwidth() / 2,
+                                        root.winfo_screenheight() / 2 - interval_popup.winfo_reqheight() / 2))
+
+    # List of predefined intervals in seconds
+    intervals = [10, 20, 30, 60, 300, 600, 1800, 3600, 14400, 86400]
+
+    # Create a button for each interval and arrange in a grid
+    for index, val in enumerate(intervals):
+        button = tk.Button(interval_popup, text=str(val), command=lambda val=val: select_interval(val))
+        button.grid(row=index // 5, column=index % 5, sticky='ew')
+
+    # Center the buttons in the pop-up window
+    interval_popup.grid_columnconfigure(0, weight=1)
+    interval_popup.grid_rowconfigure(0, weight=1)
+
+    interval_popup.grab_set()  # This makes the pop-up window modal
 
 def preview():
     # Command to preview the images using libcamera
@@ -45,19 +66,23 @@ def preview():
 root = tk.Tk()
 root.title("Time-Lapse Controller")
 
-interval = 1  # Default interval of 1 second
+# Frame for bottom left buttons
+button_frame = tk.Frame(root)
+button_frame.pack(side=tk.LEFT, anchor='sw')
+
+interval = 10  # Default interval to the first in the list
 running = False
 
 # Set interval button
-set_interval_button = tk.Button(root, text="Set Interval", command=set_interval)
-set_interval_button.pack()
+set_interval_button = tk.Button(button_frame, text="Set Interval", command=set_interval)
+set_interval_button.pack(side=tk.LEFT)
 
 # Start/Stop button
-start_stop_button = tk.Button(root, text="Start Time-Lapse", command=lambda: start_time_lapse(interval) if not running else stop_time_lapse())
-start_stop_button.pack()
+start_stop_button = tk.Button(button_frame, text="Start Time-Lapse", command=lambda: start_time_lapse(interval) if not running else stop_time_lapse())
+start_stop_button.pack(side=tk.LEFT)
 
 # Preview button
-preview_button = tk.Button(root, text="Preview", command=preview)
-preview_button.pack()
+preview_button = tk.Button(button_frame, text="Preview", command=preview)
+preview_button.pack(side=tk.LEFT)
 
 root.mainloop()
